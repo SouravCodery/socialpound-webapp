f4711e4cfb3230d12fef3fb279ba81b9300c30b5
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-12 14:16:08 +0530

    feat: addition of signServerJWT and signUserDataJWT

--- src/helpers/jwt.helpers.ts
+++ src/helpers/jwt.helpers.ts
@@ -1,15 +1,41 @@
 import jwt from "jsonwebtoken";
+import { Account, Profile } from "next-auth";
 
 const AUTH_JWT_SECRET_KEY = process.env.AUTH_JWT_SECRET_KEY ?? "";
 const AUTH_JWT_EXPIRES_IN = process.env.AUTH_JWT_EXPIRES_IN;
 
-export const signJwt = ({ user }: { user: Object }) => {
+const USER_DATA_SECRET_KEY = process.env.USER_DATA_SECRET_KEY ?? "";
+const USER_DATA_EXPIRES_IN = process.env.USER_DATA_EXPIRES_IN;
+
+export const signServerJWT = ({ user }: { user: Object }) => {
   try {
-    const token = jwt.sign(user, AUTH_JWT_SECRET_KEY, {
+    const signedServerJWT = jwt.sign(user, AUTH_JWT_SECRET_KEY, {
       expiresIn: AUTH_JWT_EXPIRES_IN,
     });
 
-    return token;
+    return signedServerJWT;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signUserDataJWT = ({
+  user,
+  account,
+  profile,
+}: {
+  user: Object;
+  account: Account | null;
+  profile?: Profile;
+}) => {
+  try {
+    const userData = { user, account: account ?? {}, profile: profile ?? {} };
+
+    const signedUserDataJWT = jwt.sign(userData, USER_DATA_SECRET_KEY, {
+      expiresIn: USER_DATA_EXPIRES_IN,
+    });
+
+    return signedUserDataJWT;
   } catch (error) {
     throw error;
   }
