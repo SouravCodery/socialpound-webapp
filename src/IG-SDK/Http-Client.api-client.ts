e2285889eb20ef488040bd44ab8a3ea50b67f684
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-14 23:10:33 +0530

    feat: error handling in request method of HttpClient

--- src/IG-SDK/Http-Client.api-client.ts
+++ src/IG-SDK/Http-Client.api-client.ts
@@ -31,35 +31,46 @@ export class HttpClient {
     queryParams?: Record<string, string | number | boolean>;
     body?: Record<string, string | number | boolean>;
   }): Promise<T> {
-    const { method = "GET" } = options || {};
+    try {
+      const { method = "GET" } = options || {};
 
-    const headers: HeadersInit = {
-      "Content-Type": "application/json",
-    };
+      const headers: HeadersInit = {
+        "Content-Type": "application/json",
+      };
 
-    if (token) {
-      headers["Authorization"] = `Bearer ${token}`;
+      if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+      }
+
+      const queryString = queryParams && this.buildQueryString(queryParams);
+
+      const url = queryString
+        ? `${this.baseURL}${endpoint}?${queryString}`
+        : `${this.baseURL}${endpoint}`;
+
+      const response = await fetch(url, {
+        method,
+        headers,
+        body: method !== "GET" ? JSON.stringify(body) : undefined,
+      });
+
+      if (!response.ok) {
+        const responseLog = response?.json ? await response.json() : response;
+
+        logger.error("Error in request: Non 2XX", responseLog);
+        throw new Error("Network response was not ok");
+      }
+
+      const responseJson = await response.json();
+
+      return responseJson;
+    } catch (error) {
+      logger.error(
+        "Error in request in HttpClient",
+        { endpoint, options, token, queryParams, body },
+        { error }
+      );
+      throw error;
     }
-
-    const queryString = queryParams && this.buildQueryString(queryParams);
-
-    const url = queryString
-      ? `${this.baseURL}${endpoint}?${queryString}`
-      : `${this.baseURL}${endpoint}`;
-
-    const response = await fetch(url, {
-      method,
-      headers,
-      body: method !== "GET" ? JSON.stringify(body) : undefined,
-    });
-
-    const responseJson = await response.json();
-
-    if (!response.ok) {
-      logger.error("Error in request: Non 2XX", responseJson);
-      throw new Error("Network response was not ok", responseJson);
-    }
-
-    return responseJson;
   }
 }
