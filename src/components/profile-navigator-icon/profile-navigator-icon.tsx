e3134c2b80257233959f3f3f93b4ae30ffd11c47
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-06-13 00:31:47 +0530

    feat: upScale in ProfileNavigatorIcon

--- src/components/profile-navigator-icon/profile-navigator-icon.tsx
+++ src/components/profile-navigator-icon/profile-navigator-icon.tsx
@@ -1,17 +1,22 @@
+import clsx from "clsx";
 import Image from "next/image";
 
 import auth from "@/middleware";
 
 import classes from "./profile-navigator-icon.module.css";
 
-export const ProfileNavigatorIcon = async () => {
+export const ProfileNavigatorIcon = async ({
+  upScale = false,
+}: {
+  upScale?: Boolean;
+}) => {
   const session = await auth();
   const dp = session?.user?.image;
 
   if (!dp) return null;
 
   return (
-    <div className={classes.dpContainer}>
+    <div className={clsx(classes.dpContainer, upScale && classes.upScale)}>
       <Image src={dp} alt="Profile Picture" fill sizes="60px" />
     </div>
   );
