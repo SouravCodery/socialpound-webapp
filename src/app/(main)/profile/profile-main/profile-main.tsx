89f9d98b35b7f8bb36d3b63db585a2fd95829af3
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-06-21 21:44:12 +0530

    feat: ProfileMain header tsx and styling

--- src/app/(main)/profile/profile-main/profile-main.tsx
+++ src/app/(main)/profile/profile-main/profile-main.tsx
@@ -1,3 +1,23 @@
+import { IconWrapper } from "@/components/atoms/icon-wrapper/icon-wrapper";
+import { PostsIcon, ReelsIcon, TaggedIcon } from "@/components/icons/icons";
+
+import classes from "./profile-main.module.css";
+
 export const ProfileMain = () => {
-  return <div>Profile Main</div>;
+  return (
+    <div className={classes.profileMain}>
+      <div className={classes.profileMainHeader}>
+        <IconWrapper>
+          <PostsIcon />
+        </IconWrapper>
+        <IconWrapper>
+          <ReelsIcon />
+        </IconWrapper>
+        <IconWrapper>
+          <TaggedIcon />
+        </IconWrapper>
+      </div>
+      <div className={classes.profileMainBody}>Main</div>
+    </div>
+  );
 };
