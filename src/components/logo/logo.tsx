c1e0898c6c527b673ae973d5e47a034966d553b9
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-07-05 01:56:53 +0530

    feat: Logo tsx and css module

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import { Cedarville_Cursive } from "next/font/google";
+
+import classes from "./logo.module.css";
+
+const cedarvilleCursive = Cedarville_Cursive({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const Logo = () => {
+  return (
+    <div className={clsx(classes.logo, cedarvilleCursive.className)}>
+      Socialgram
+    </div>
+  );
+};
