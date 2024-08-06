1ddcd9dadeadf436331e2133e339d4cdc023700c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-07 01:41:55 +0530

    feat: Make API_BASE_URL NEXT_PUBLIC_API_BASE_URL

--- src/ig-sdk/ig-sdk.instance.ts
+++ src/ig-sdk/ig-sdk.instance.ts
@@ -1,5 +1,5 @@
 import { IG_SDK } from "./ig-sdk";
 
-const baseURL = process.env.API_BASE_URL ?? "";
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
 
 export const apiSDKInstance = new IG_SDK(baseURL);
