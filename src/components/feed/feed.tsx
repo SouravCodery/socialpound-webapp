29f793c44605e23dfd583c4a108a624d50ec985d
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-08 21:09:11 +0530

    feat: Integrate PostLoader in Feed

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -6,12 +6,19 @@ import classes from "./feed.module.css";
 import { Post } from "@/components/post/post";
 
 import { useSWRFetchPosts } from "@/hooks/swr-hooks/post.swr-hooks";
+import { PostLoader } from "../loaders/post/post-loader";
 
 export default function Feed() {
   const { posts, error, isLoading } = useSWRFetchPosts();
 
   if (isLoading) {
-    return <div>Loading!</div>;
+    return (
+      <div className={clsx(classes.feed)}>
+        {[...Array(2)].map((_, index) => (
+          <PostLoader key={index} />
+        ))}
+      </div>
+    );
   }
 
   return (
