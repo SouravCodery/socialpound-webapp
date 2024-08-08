8ffb344fea78cffa83e12e0145bdce070f3b4d8a
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-08 16:57:15 +0530

    feat: Integrate PostModule into SDK

--- src/ig-sdk/ig-sdk.ts
+++ src/ig-sdk/ig-sdk.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from "./http-client.api-client";
+
 import { UserModule } from "./modules/user-module";
+import { PostModule } from "./modules/post-module";
 
 export class IG_SDK {
-  public user: UserModule;
   public baseUrl: string;
 
+  public user: UserModule;
+  public post: PostModule;
+
   constructor(baseURL: string) {
+    this.baseUrl = baseURL;
     const httpClient = new HttpClient(baseURL);
 
-    this.baseUrl = baseURL;
     this.user = new UserModule(httpClient);
+    this.post = new PostModule(httpClient);
   }
 }
