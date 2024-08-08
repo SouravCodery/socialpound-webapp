a6f2cda76afad919cdc7863c3b07b834dd5a31a3
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-08 23:13:39 +0530

    feat: Add useSWRAddPost

--- src/hooks/swr-hooks/post.swr-hooks.ts
+++ src/hooks/swr-hooks/post.swr-hooks.ts
@@ -1,7 +1,9 @@
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
 
 import { API_ROUTES } from "@/ig-sdk/api-routes";
 import { apiSDKInstance } from "@/ig-sdk/ig-sdk.instance";
+import { PostInterface } from "@/models/interfaces/post.interface";
 
 export const useSWRFetchPosts = () => {
   const { data, error, isLoading } = useSWR(API_ROUTES.post.createPost, () =>
@@ -13,4 +15,26 @@ export const useSWRFetchPosts = () => {
     error,
     isLoading,
   };
+};
+
+export const useSWRAddPost = () => {
+  const { trigger, error, isMutating, data } = useSWRMutation(
+    API_ROUTES.post.createPost,
+    (
+      _,
+      {
+        arg,
+      }: {
+        arg: {
+          content: PostInterface["content"];
+          caption: PostInterface["caption"];
+        };
+      }
+    ) => {
+      const { content, caption } = arg;
+      return apiSDKInstance.post.createPost({ content, caption });
+    }
+  );
+
+  return { isMutating, trigger, error, data };
 };
