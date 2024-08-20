9a3b7c5bb908b59806146c86e5e009b56f0faf9b
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-21 03:04:51 +0530

    feat: Add addComment and getCommentsByPostId url

--- src/ig-sdk/api-routes.ts
+++ src/ig-sdk/api-routes.ts
@@ -6,4 +6,9 @@ export const API_ROUTES = {
     createPost: "/v1/post",
     fetchPosts: "/v1/post",
   },
+  comment: {
+    addComment: "/v1/comment",
+    getCommentsByPostId: ({ postId }: { postId: string }) =>
+      `/v1/comment/post/${postId}`,
+  },
 } as const;
