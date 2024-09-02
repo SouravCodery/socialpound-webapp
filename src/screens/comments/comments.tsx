fc2d5fb8f7134d1d161ab6320874346b0c654701
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-02 16:19:13 +0530

    feat: Apply CommentLoader in Comments

--- src/screens/comments/comments.tsx
+++ src/screens/comments/comments.tsx
@@ -3,6 +3,7 @@
 import classes from "./comments.module.css";
 import { useSWRGetCommentsByPostId } from "@/hooks/swr-hooks/comment.swr-hooks";
 import { Comment } from "./comment/comment";
+import { CommentLoader } from "@/components/loaders/comments/comment-loader";
 
 export const Comments = ({ postId }: { postId: string }) => {
   const { data, error, size, setSize, isLoading, isNextPageAvailable } =
@@ -11,7 +12,13 @@ export const Comments = ({ postId }: { postId: string }) => {
     });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={classes.comments}>
+        {[...Array(5)].map((_, index) => (
+          <CommentLoader key={index} />
+        ))}
+      </div>
+    );
   }
 
   return (
