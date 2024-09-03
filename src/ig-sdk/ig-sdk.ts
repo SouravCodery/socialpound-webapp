94d3da0195fbdc6a0d7cf2a298bddbf7d6ecb8d0
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-04 00:03:51 +0530

    feat: Integrate AWSPresignedUrlModule in IG_SDK

--- src/ig-sdk/ig-sdk.ts
+++ src/ig-sdk/ig-sdk.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "./http-client.api-client";
 import { UserModule } from "./modules/user-module";
 import { PostModule } from "./modules/post-module";
 import { CommentModule } from "./modules/comment-module";
+import { AWSPresignedUrlModule } from "./modules/aws-presigned-url-module";
 
 export class IG_SDK {
   public baseUrl: string;
@@ -10,6 +11,7 @@ export class IG_SDK {
   public user: UserModule;
   public post: PostModule;
   public comment: CommentModule;
+  public awsPresignedUrl: AWSPresignedUrlModule;
 
   constructor(baseURL: string) {
     this.baseUrl = baseURL;
@@ -18,5 +20,6 @@ export class IG_SDK {
     this.user = new UserModule(httpClient);
     this.post = new PostModule(httpClient);
     this.comment = new CommentModule(httpClient);
+    this.awsPresignedUrl = new AWSPresignedUrlModule(httpClient);
   }
 }
