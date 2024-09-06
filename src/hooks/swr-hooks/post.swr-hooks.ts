8d5006d0838f4ba78c430a96e538d946d8b0a7ab
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-06 13:20:28 +0530

    feat: Add useSWRGetUserFeed hook

--- src/hooks/swr-hooks/post.swr-hooks.ts
+++ src/hooks/swr-hooks/post.swr-hooks.ts
@@ -1,19 +1,48 @@
 import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 import useSWRMutation from "swr/mutation";
 
 import { API_ROUTES } from "@/ig-sdk/api-routes";
 import { apiSDKInstance } from "@/ig-sdk/ig-sdk.instance";
 import { PostInterface } from "@/models/interfaces/post.interface";
 
-export const useSWRFetchPosts = () => {
-  const { data, error, isLoading } = useSWR(API_ROUTES.post.createPost, () =>
-    apiSDKInstance.post.fetchPosts()
+export const useSWRGetUserFeed = () => {
+  const getKey = (pageIndex: number, previousPageData: any) => {
+    if (pageIndex === 0) {
+      return API_ROUTES.post.getUserFeed;
+    }
+
+    if (!previousPageData || !previousPageData?.nextCursor) {
+      return null;
+    }
+
+    return (
+      API_ROUTES.post.getUserFeed + `?cursor=${previousPageData.nextCursor}`
+    );
+  };
+
+  const { data, error, isLoading, size, setSize, mutate } = useSWRInfinite(
+    getKey,
+    (key) => {
+      const cursor = key.split("?cursor=")[1] || "";
+
+      return apiSDKInstance.post.getUserFeed({
+        cursor,
+      });
+    }
   );
 
+  const isNextPageAvailable = data?.[size - 1]?.nextCursor !== null;
+  const isNextPageLoading = data?.[size - 1]?.posts === undefined;
+
   return {
-    posts: data,
+    data,
     error,
     isLoading,
+    setSize,
+    isNextPageAvailable,
+    isNextPageLoading,
+    updatePosts: mutate,
   };
 };
 
