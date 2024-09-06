a5790c440cfe8c9c2f0453d1a40d127252a0afcb
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-06 22:23:44 +0530

    feat: Remove Explore, Reels in Navbar and Add active styling

--- src/components/layout/primary-routes/mobile-navbar-primary-routes/mobile-navbar-primary-routes.tsx
+++ src/components/layout/primary-routes/mobile-navbar-primary-routes/mobile-navbar-primary-routes.tsx
@@ -1,38 +1,51 @@
+"use client";
+
 import Link from "next/link";
-import { Suspense } from "react";
+import { usePathname } from "next/navigation";
 
 import clsx from "clsx";
 import classes from "./mobile-navbar-primary-routes.module.css";
 
 import {
   HomeIcon,
-  ExploreIcon,
+  // ExploreIcon,
   HomePostIcon,
-  ReelsIcon,
+  // ReelsIcon,
 } from "@/components/icons/icons";
 import { IconWrapper } from "@/components/atoms/icon-wrapper/icon-wrapper";
 import { AuthUserProfilePicture } from "@/components/auth-user-profile-picture/auth-user-profile-picture";
 
 const routes = [
-  { path: "/", name: "Home", icon: HomeIcon },
-  { path: "/explore", name: "Search", icon: ExploreIcon },
+  { path: "/", name: "Home", icon: HomeIcon, count: 4 },
+  // { path: "/explore", name: "Search", icon: ExploreIcon },
   { path: "/new-post", name: "New Post", icon: HomePostIcon },
-  { path: "/reels", name: "Reels", icon: ReelsIcon, count: 2 },
-  { path: "/profile", name: "Profile", icon: AuthUserProfilePicture },
+  // { path: "/reels", name: "Reels", icon: ReelsIcon, count: 2 },
+  {
+    path: "/profile",
+    name: "Profile",
+    icon: AuthUserProfilePicture,
+  },
 ];
 
-export const MobileNavbar = async () => {
+export const MobileNavbar = () => {
+  const pathName = usePathname();
+
   return (
     <footer className={clsx(classes.footer)}>
       <nav className={clsx(classes.navbar)}>
         {routes.map((route) => (
-          <Suspense key={route.path} fallback={null}>
-            <Link href={route.path} className={classes.link}>
-              <IconWrapper count={route.count}>
-                <route.icon />
-              </IconWrapper>
-            </Link>
-          </Suspense>
+          <Link
+            key={route.path}
+            href={route.path}
+            className={clsx(
+              classes.link,
+              pathName === route.path && classes.active
+            )}
+          >
+            <IconWrapper count={route.count}>
+              <route.icon />
+            </IconWrapper>
+          </Link>
         ))}
       </nav>
     </footer>
