da79eb6a3ab37594f315fcc04af5825e67a4a570
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-06 19:34:54 +0530

    feat: Add back-button in secondary header

--- src/components/layout/secondary-routes/header-secondary-routes/header-secondary-routes.tsx
+++ src/components/layout/secondary-routes/header-secondary-routes/header-secondary-routes.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+
 import classes from "./header-secondary-routes.module.css";
 
+import { GoBackIcon } from "@/components/icons/icons";
+
 export const HeaderSecondaryRoutes = async () => {
+  const router = useRouter();
   const pathName = usePathname();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   const currentRoute = (pathName?.split("/")?.[1] ?? "").replace("-", " ");
 
-  return <header className={classes.header}>{currentRoute}</header>;
+  return (
+    <header className={classes.header}>
+      <button className={classes.backButton} onClick={handleGoBack}>
+        <GoBackIcon />
+      </button>
+      {currentRoute}
+    </header>
+  );
 };
