a54d2f2573d61637b9bce3922e1f9be694d46bc4
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-07 21:02:48 +0530

    feat: Add useSWRGetUserByUsername

--- src/hooks/swr-hooks/user.swr-hooks.ts
+++ src/hooks/swr-hooks/user.swr-hooks.ts
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 
 import { apiSDKInstance } from "@/ig-sdk/ig-sdk.instance";
+import { API_ROUTES } from "@/ig-sdk/api-routes";
 
 export const useSWRGetDecodedUserToken = () => {
   const { data, error, isLoading, mutate } = useSWR(
@@ -11,7 +12,23 @@ export const useSWRGetDecodedUserToken = () => {
   );
 
   return {
-    user: data,
+    userDecodedToken: data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export const useSWRGetUserByUsername = ({ username }: { username: string }) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    API_ROUTES.user.getUserByUsername({ username }),
+    () => {
+      return apiSDKInstance.user.getUserByUsername({ username });
+    }
+  );
+
+  return {
+    user: data?.data?.user,
     error,
     isLoading,
     mutate,
