be8e4534920ca67bceb9092f29cdde59bbaba8e6
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-07 14:39:46 +0530

    feat: Add loading fallback in CSROnlyHOC

--- src/components/csr-only-hoc/csr-only-hoc.tsx
+++ src/components/csr-only-hoc/csr-only-hoc.tsx
@@ -1,9 +1,17 @@
 import dynamic from "next/dynamic";
 
-const CSROnly = dynamic(() => import("./_csr-only"), {
-  ssr: false,
-});
+const _CSROnly = ({
+  loadingFallbackComponent,
+}: {
+  loadingFallbackComponent?: React.ReactElement;
+}) =>
+  dynamic(() => import("./_csr-only"), {
+    ssr: false,
+    loading: () => loadingFallbackComponent ?? null,
+  });
 
 export const CSROnlyHOC = ({ component }: { component: React.ReactNode }) => {
+  const CSROnly = _CSROnly({});
+
   return <CSROnly component={component} />;
 };
