38115651543d5f7296389b2b129f2e8a30e6c798
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-08 00:01:19 +0530

    feat: Add username in ProfilePage

--- src/app/(primary-routes)/profile/[username]/page.tsx
+++ src/app/(primary-routes)/profile/[username]/page.tsx
@@ -1,5 +1,11 @@
 import { Profile } from "@/screens/profile/profile";
 
-export default function ProfilePage() {
-  return <Profile />;
+export default function ProfilePage({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const { username } = params;
+
+  return <Profile username={username} />;
 }
