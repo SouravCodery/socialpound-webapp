2b133bafba75c885acfaeee5f2472819b50ca945
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-07 21:05:14 +0530

    feat: Add useSWRGetUserByUsername, useSWRGetDecodedUserToken in Profile

--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import clsx from "clsx";
 import classes from "./profile.module.css";
 
@@ -5,10 +7,26 @@ import { LogoutButton } from "@/components/auth/auth";
 import { ProfileHeader } from "./profile-header/profile-header";
 import { ProfileMain } from "./profile-main/profile-main";
 
+import {
+  useSWRGetUserByUsername,
+  useSWRGetDecodedUserToken,
+} from "@/hooks/swr-hooks/user.swr-hooks";
+
 export const Profile = () => {
+  const { user, isLoading } = useSWRGetUserByUsername({
+    username: "souravscchoudhary",
+  });
+  const { userDecodedToken } = useSWRGetDecodedUserToken();
+
+  const isOwnProfile = user?.email === userDecodedToken?.email;
+
   return (
     <div className={clsx(classes.profile)}>
-      <ProfileHeader />
+      <ProfileHeader
+        user={user}
+        isLoading={isLoading}
+        isOwnProfile={isOwnProfile}
+      />
       <ProfileMain />
 
       <br />
