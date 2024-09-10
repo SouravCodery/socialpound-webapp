42d08ec1263f622f8ba4be215945ae09942e0e02
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-10 16:13:44 +0530

    feat: Update CommentLoader component and styling to align it with updated Comment

--- src/components/loaders/comments/comment-loader.tsx
+++ src/components/loaders/comments/comment-loader.tsx
@@ -2,14 +2,13 @@ import classes from "./comment-loader.module.css";
 
 export const CommentLoader = () => {
   return (
-    <div className={classes.loader}>
+    <div className={classes.comment}>
       <div className={classes.main}>
-        <div className={classes.dpContainer}></div>
+        <div className={classes.skeletonProfilePicture}></div>
         <div className={classes.content}>
-          <div className={classes.usernameContainer}></div>
-          <div className={classes.textContainer}></div>
+          <div className={classes.skeletonUsername}></div>
+          <div className={classes.skeletonText}></div>
         </div>
-        <div className={classes.likeContainer}></div>
       </div>
     </div>
   );
