7bc9fe71e643f0898c63205df0d1e22b6845e85a
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-11 17:34:47 +0530

    feat: Add unlikeUserPost in LikeButton

--- src/components/like-button/like-button.tsx
+++ src/components/like-button/like-button.tsx
@@ -3,25 +3,30 @@ import Link from "next/link";
 import classes from "./like-button.module.css";
 
 import { LikeIcon, UnlikeIcon } from "../icons/icons";
-import { isPostLikedByUser, likeUserPost } from "@/services/like.services";
+import {
+  isPostLikedByUser,
+  likeUserPost,
+  unlikeUserPost,
+} from "@/services/like.services";
 
 export const LikeButton = ({
-  // likeOn,
   postId,
-  // commentId,
   count,
 }: {
-  // likeOn: "Post" | "Comment";
   postId: string;
-  // commentId?: string;
   count: number;
 }) => {
   const url = `/likes/${postId}`;
 
   const isLiked = isPostLikedByUser({ postId });
   const [isPostLiked, setIsPostLiked] = useState(isLiked);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const likePost = async () => {
+    if (isProcessing) return;
+
+    setIsProcessing(true);
+
     if (isPostLiked === false) {
       setIsPostLiked(true);
       await likeUserPost({ postId });
@@ -29,8 +34,9 @@ export const LikeButton = ({
     }
 
     setIsPostLiked(false);
-    // await unlikeUserPost({ postId });
-    // return;
+    await unlikeUserPost({ postId });
+
+    setIsProcessing(false);
   };
 
   return (
