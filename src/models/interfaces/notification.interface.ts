f27378b43771010e66c777a9418af79499a10365
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-13 21:06:54 +0530

    feat: Add NotificationTypes in notification.interface

--- src/models/interfaces/notification.interface.ts
+++ src/models/interfaces/notification.interface.ts
@@ -1,15 +1,25 @@
 import { PostInterface } from "@/models/interfaces/post.interface";
 import { SubDocumentUserInterface } from "./user.interface";
 
+type NotificationTypes =
+  | "like-on-post"
+  | "like-on-comment"
+  | "comment"
+  | "reply"
+  | "add-friend";
+
 export interface NotificationInterface {
   _id: string;
   sender: SubDocumentUserInterface;
-  type: string;
+  type: NotificationTypes;
   post: {
     _id: string;
-    content: PostInterface["content"][];
+    content: PostInterface["content"];
   };
-  comment: string;
+  comment?: {
+    _id: string;
+    text: string;
+  };
   read: boolean;
 }
 
