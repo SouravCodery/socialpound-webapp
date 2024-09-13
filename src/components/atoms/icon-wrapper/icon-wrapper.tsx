bf6685bd266a05ccdf4c4443f9f08eb9f970af4c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-13 21:05:16 +0530

    feat: Add dot in IconWrapper

--- src/components/atoms/icon-wrapper/icon-wrapper.tsx
+++ src/components/atoms/icon-wrapper/icon-wrapper.tsx
@@ -1,17 +1,21 @@
+import clsx from "clsx";
 import classes from "./icon-wrapper.module.css";
 
 export const IconWrapper = ({
   children,
   count,
+  dot = false,
 }: {
   children: React.ReactNode;
-  count?: number | undefined;
+  count?: number;
+  dot?: boolean;
 }) => {
   return (
     <div className={classes.iconWrapper}>
       <div className={classes.iconWrapperChild}>
         {children}
         {count && <div className={classes.count}>{count}</div>}
+        {dot && <div className={classes.dot} />}
       </div>
     </div>
   );
