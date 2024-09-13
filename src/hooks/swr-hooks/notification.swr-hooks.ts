c719f57a76e9447ad3f459d11c4e3df63f6e89db
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-13 19:24:59 +0530

    feat: Add useSWRGetNotificationsByPostId

---
+++ src/hooks/swr-hooks/notification.swr-hooks.ts
@@ -0,0 +1,50 @@
+import useSWRInfinite from "swr/infinite";
+
+import { API_ROUTES } from "@/ig-sdk/api-routes";
+import { apiSDKInstance } from "@/ig-sdk/ig-sdk.instance";
+
+import { NotificationResponseInterface } from "@/models/interfaces/notification.interface";
+
+export const useSWRGetNotificationsByPostId = () => {
+  const getKey = (
+    pageIndex: number,
+    previousPageData: NotificationResponseInterface
+  ) => {
+    if (pageIndex === 0) {
+      return API_ROUTES.notification.getNotificationsByUser;
+    }
+
+    if (!previousPageData || !previousPageData?.nextCursor) {
+      return null;
+    }
+
+    return (
+      API_ROUTES.notification.getNotificationsByUser +
+      `?cursor=${previousPageData.nextCursor}`
+    );
+  };
+
+  const { data, error, isLoading, size, setSize, mutate } = useSWRInfinite(
+    getKey,
+    (key) => {
+      const cursor = key.split("?cursor=")[1] || "";
+
+      return apiSDKInstance.notification.getNotificationsByUser({
+        cursor,
+      });
+    }
+  );
+
+  const isNextPageAvailable = data?.[size - 1]?.nextCursor !== null;
+  const isNextPageLoading = data?.[size - 1]?.notifications === undefined;
+
+  return {
+    data,
+    error,
+    isLoading,
+    setSize,
+    isNextPageAvailable,
+    isNextPageLoading,
+    updateNotifications: mutate,
+  };
+};
