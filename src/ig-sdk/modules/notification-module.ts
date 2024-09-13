ec18d1352247a7763ad094c6989e54b25e22ef19
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-13 19:19:44 +0530

    feat: Add getNotificationsByPostId, NotificationModule

---
+++ src/ig-sdk/modules/notification-module.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "../http-client.api-client";
+import { API_ROUTES } from "../api-routes";
+
+import { NotificationResponseInterface } from "@/models/interfaces/notification.interface";
+
+export class NotificationModule {
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  async getNotificationsByPostId({ cursor }: { cursor: string }) {
+    const fetchNotificationsResponse =
+      await this.httpClient.request<NotificationResponseInterface>({
+        endpoint: API_ROUTES.notification.getNotificationsByUser,
+        options: {
+          method: "GET",
+        },
+        body: {},
+        queryParams: { cursor },
+        token: await this.httpClient.getToken(),
+      });
+
+    return fetchNotificationsResponse.data;
+  }
+}
