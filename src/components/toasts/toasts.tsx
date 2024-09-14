15b4f0471f1e5aecac362233cf0ee5c9d87b1a2c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-14 13:51:44 +0530

    feat: Integrate Toast in Toasts and change styles

--- src/components/toasts/toasts.tsx
+++ src/components/toasts/toasts.tsx
@@ -1,10 +1,31 @@
-import clsx from "clsx";
+"use client";
+
 import classes from "./toasts.module.css";
+import { Toast } from "./toast/toast";
+import { useEffect, useState } from "react";
 
 export const Toasts = () => {
+  const [toasts, setToasts] = useState<string[]>([
+    "Your comment will reflect soon.",
+    "You have a new follower.",
+    "Your post has received over 100k likes.",
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (toasts.length > 0) {
+        setToasts((prev) => prev.slice(1));
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [toasts]);
+
   return (
-    <div className={clsx(classes.toasts, "shadow")}>
-      Your comment will reflect soon! Your comment will reflect soon!
+    <div className={classes.toasts}>
+      {toasts.map((toast, index) => (
+        <Toast key={toast} message={toast} index={index} />
+      ))}
     </div>
   );
 };
