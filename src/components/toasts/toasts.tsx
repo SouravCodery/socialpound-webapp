b7c35b4d688e6e2ecc1fbdfd802f07e9362d5d6e
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-14 17:43:09 +0530

    feat: Change types in Toasts

--- src/components/toasts/toasts.tsx
+++ src/components/toasts/toasts.tsx
@@ -4,14 +4,14 @@ import eventEmitter, { Emitter } from "mitt";
 
 import classes from "./toasts.module.css";
 import { Toast } from "./toast/toast";
-import { ToastType } from "@/models/types/toast.types";
+import { GenericToastType } from "@/models/types/toast.types";
 
-const emitter: Emitter<ToastType> = eventEmitter();
+const emitter: Emitter<GenericToastType> = eventEmitter();
 
 export const Toasts = () => {
-  const [toasts, setToasts] = useState<ToastType["bakeToast"][]>([]);
+  const [toasts, setToasts] = useState<GenericToastType["bakeToast"][]>([]);
 
-  const addToast = (toast: ToastType["bakeToast"]) => {
+  const addToast = (toast: GenericToastType["bakeToast"]) => {
     setToasts((prev) => [toast, ...prev]);
   };
 
@@ -32,8 +32,8 @@ export const Toasts = () => {
 
   return (
     <div className={classes.toasts}>
-      {toasts.map(({ key, message }, index) => (
-        <Toast key={key} message={message} index={index} />
+      {toasts.map((toast, index) => (
+        <Toast key={toast.key} toast={toast} index={index} />
       ))}
     </div>
   );
@@ -43,7 +43,7 @@ export const bakeToast = ({
   type = "info",
   message,
 }: {
-  type?: ToastType["bakeToast"]["type"];
+  type?: GenericToastType["bakeToast"]["type"];
   message: string;
 }) => {
   emitter.emit("bakeToast", { type, message, key: `${message}${Date.now()}` });
