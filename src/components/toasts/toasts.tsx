96d835a48f2a4ac4fa4589d8f46d951e362b1451
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-14 15:43:00 +0530

    feat: Integrate mitt in Toasts, Add emitters

--- src/components/toasts/toasts.tsx
+++ src/components/toasts/toasts.tsx
@@ -1,31 +1,50 @@
 "use client";
+import { useEffect, useState } from "react";
+import eventEmitter, { Emitter } from "mitt";
 
 import classes from "./toasts.module.css";
 import { Toast } from "./toast/toast";
-import { useEffect, useState } from "react";
+import { ToastType } from "@/models/types/toast.types";
+
+const emitter: Emitter<ToastType> = eventEmitter();
 
 export const Toasts = () => {
-  const [toasts, setToasts] = useState<string[]>([
-    "Your comment will reflect soon.",
-    "You have a new follower.",
-    "Your post has received over 100k likes.",
-  ]);
+  const [toasts, setToasts] = useState<ToastType["bakeToast"][]>([]);
+
+  const addToast = (toast: ToastType["bakeToast"]) => {
+    setToasts((prev) => [toast, ...prev]);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (toasts.length > 0) {
         setToasts((prev) => prev.slice(1));
       }
-    }, 3000);
+    }, 2500);
 
-    return () => clearInterval(interval);
+    emitter.on("bakeToast", addToast);
+
+    return () => {
+      clearInterval(interval);
+      emitter.off("bakeToast", addToast);
+    };
   }, [toasts]);
 
   return (
     <div className={classes.toasts}>
-      {toasts.map((toast, index) => (
-        <Toast key={toast} message={toast} index={index} />
+      {toasts.map(({ key, message }, index) => (
+        <Toast key={key} message={message} index={index} />
       ))}
     </div>
   );
+};
+
+export const bakeToast = ({
+  type = "info",
+  message,
+}: {
+  type: ToastType["bakeToast"]["type"];
+  message: string;
+}) => {
+  emitter.emit("bakeToast", { type, message, key: `${message}${Date.now()}` });
 };
