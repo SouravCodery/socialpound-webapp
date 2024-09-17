a7b87749741a9ac5ee4eae1023d171c2fe6829bf
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-17 16:17:32 +0530

    feat: Add Modal component and styling

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,53 @@
+import clsx from "clsx";
+import classes from "./modal.module.css";
+
+import { Spinner } from "../loaders/spinner/spinner";
+
+//todo: render this component in a modal div of layout, maybe using ReactDOM.createPortal
+export const Modal = ({
+  title,
+  message,
+  confirmationButtonText,
+  processingText,
+
+  isProcessing,
+  closeModal,
+}: {
+  title: string;
+  message: string;
+  confirmationButtonText: string;
+  processingText: string;
+
+  isProcessing: boolean;
+  closeModal: () => void;
+}) => {
+  return (
+    <>
+      <div className={classes.curtain} onClick={closeModal} />
+      <div className={classes.confirmation}>
+        <div className={classes.header}>
+          <div className={classes.title}>{title}</div>
+          <div className={classes.message}>{message}</div>
+        </div>
+        <div className={classes.processing}>{isProcessing && <Spinner />}</div>
+        <div className={classes.actions}>
+          <button
+            className={clsx(classes.actionButton, classes.confirm)}
+            onClick={closeModal}
+            disabled={isProcessing}
+          >
+            {isProcessing === false ? confirmationButtonText : processingText}
+          </button>
+          {isProcessing === false && (
+            <button
+              className={clsx(classes.actionButton, classes.cancel)}
+              onClick={closeModal}
+            >
+              Cancel
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
