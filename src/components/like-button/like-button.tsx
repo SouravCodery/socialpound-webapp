93b2dcb334c69453429adeba1d9515646a5cf5a7
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-18 14:17:38 +0530

    feat: Change LikeButton structure and styling

--- src/components/like-button/like-button.tsx
+++ src/components/like-button/like-button.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import classes from "./like-button.module.css";
 
 import { LikeIcon, UnlikeIcon } from "../icons/icons";
@@ -17,12 +16,13 @@ export const LikeButton = ({
   const url = `/likes/${postId}`;
 
   return (
-    <button className={classes.button} onClick={likePost}>
-      {isPostLiked === false ? <LikeIcon /> : <UnlikeIcon />}
-
-      <Link href={url} className={classes.link}>
-        <div className={classes.counter}>{count > 0 ? count : null}</div>
-      </Link>
-    </button>
+    <>
+      <button className={classes.button} onClick={likePost}>
+        {isPostLiked === false ? <LikeIcon /> : <UnlikeIcon />}
+      </button>
+      <div title="Like Count" className={classes.counter}>
+        {count > 0 ? count : null}
+      </div>
+    </>
   );
 };
