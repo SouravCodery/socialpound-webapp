069574cb0487a85ed5504773db0681e5b8bb3722
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-19 19:22:18 +0530

    feat: Remove CSROnlyHOC from AuthUserProfilePicture

--- src/components/auth-user-profile-picture/auth-user-profile-picture.tsx
+++ src/components/auth-user-profile-picture/auth-user-profile-picture.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { ProfilePicture } from "../profile-picture/profile-picture";
-import { CSROnlyHOC } from "../csr-only-hoc/csr-only-hoc";
 import { useSWRGetDecodedUserToken } from "@/hooks/swr-hooks/user.swr-hooks";
-
-const _AuthUserProfilePicture = ({ scale }: { scale?: "medium" | "large" }) => {
-  const { userDecodedToken } = useSWRGetDecodedUserToken();
-
-  return <ProfilePicture dpURL={userDecodedToken?.image ?? ""} scale={scale} />;
-};
 
 export const AuthUserProfilePicture = ({
   scale,
 }: {
   scale?: "medium" | "large";
-}) => <CSROnlyHOC component={<_AuthUserProfilePicture scale={scale} />} />;
+}) => {
+  const { userDecodedToken } = useSWRGetDecodedUserToken();
+
+  return <ProfilePicture dpURL={userDecodedToken?.image ?? ""} scale={scale} />;
+};
