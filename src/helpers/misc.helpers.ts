4242858e5ef0fb02ead455374b858729bc8f7435
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-21 17:06:05 +0530

    feat: Add isRunningOnClient helper

--- src/helpers/misc.helpers.ts
+++ src/helpers/misc.helpers.ts
@@ -1,2 +1,4 @@
 export const delay = ({ duration = 1000 }: { duration?: number }) =>
   new Promise((resolve) => setTimeout(resolve, duration));
+
+export const isRunningOnClient = () => typeof window !== "undefined";
