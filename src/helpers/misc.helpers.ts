e847b90e44a35b723c70344b251c24b26d5d5047
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-21 17:19:19 +0530

    feat: Add isDevEnvironment helper

--- src/helpers/misc.helpers.ts
+++ src/helpers/misc.helpers.ts
@@ -2,3 +2,5 @@ export const delay = ({ duration = 1000 }: { duration?: number }) =>
   new Promise((resolve) => setTimeout(resolve, duration));
 
 export const isRunningOnClient = () => typeof window !== "undefined";
+
+export const isDevEnvironment = () => process.env.NODE_ENV === "development";
