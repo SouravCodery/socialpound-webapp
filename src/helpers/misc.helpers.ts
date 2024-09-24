4e41b34537ad65d8036606c9991e6d88bb698536
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-24 19:11:18 +0530

    feat: Add convertStringDaysToSeconds helper

--- src/helpers/misc.helpers.ts
+++ src/helpers/misc.helpers.ts
@@ -6,3 +6,7 @@ export const isRunningOnClient = () => typeof window !== "undefined";
 export const isDevEnvironment = () => process.env.NODE_ENV === "development";
 
 export const trimUsername = (username: string = "") => username.split("@")[0];
+
+export const convertStringDaysToSeconds = ({ key }: { key: String }) => {
+  return Number(key.replace("d", "")) * 24 * 60 * 60;
+};
