c9635ebcd750804e65a1ff30fb9791ffd69c161b
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-24 19:04:07 +0530

    fix: Import of convertStringDaysToSeconds

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import { cookies } from "next/headers";
-import * as jwtHelpers from "@/helpers/jwt-server-side.helpers";
+import { convertStringDaysToSeconds } from "@/helpers/misc.helpers";
 import { logger } from "@/logger/index.logger";
 
 export async function setServerToken({ token }: { token: string }) {
   try {
     const AUTH_JWT_EXPIRES_IN = process.env.AUTH_JWT_EXPIRES_IN || "30d";
-    const maxAgeInSeconds = jwtHelpers.convertStringDaysToSeconds({
+    const maxAgeInSeconds = convertStringDaysToSeconds({
       key: AUTH_JWT_EXPIRES_IN,
     });
 
