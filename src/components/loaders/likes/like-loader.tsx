4525ce87fe8e2ca78198a75e3293a8545aef7f1f
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-30 23:23:03 +0530

    style: Add shadow and remove border in LikeLoader

--- src/components/loaders/likes/like-loader.tsx
+++ src/components/loaders/likes/like-loader.tsx
@@ -1,8 +1,9 @@
+import clsx from "clsx";
 import classes from "./like-loader.module.css";
 
 export const LikeLoader = () => {
   return (
-    <div className={classes.like}>
+    <div className={clsx(classes.like, "shadow")}>
       <div className={classes.skeletonProfilePicture}></div>
       <div className={classes.content}>
         <div className={classes.skeletonUsername}></div>
