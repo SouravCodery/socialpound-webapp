df3e073629398a78d66a2544ade108339e9bbcf4
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-01 20:01:37 +0530

    feat: Add TermsOfService page and screen

---
+++ src/app/public/terms-of-service/page.tsx
@@ -0,0 +1,5 @@
+import { TermsOfService } from "@/screens/public/terms-of-service/terms-of-service";
+
+export default function TermsOfServicePage() {
+  return <TermsOfService />;
+}
---
+++ src/screens/public/terms-of-service/terms-of-service.tsx
@@ -0,0 +1,17 @@
+import { InfoPage } from "@/components/info-page/info-page";
+
+export const TermsOfService = () => {
+  return <InfoPage title="Terms of Service" content={termsOfServiceContent} />;
+};
+
+const termsOfServiceContent = [
+  "Welcome to Socialpound! By accessing or using this website, you agree to comply with and be bound by the following terms and conditions. If you do not agree to these terms, please do not use the platform.",
+  "1. Purpose of the Platform: Socialpound is a learning platform created to practice the tech stack it is built on, as well as explore various components of system design while building social media features.",
+  "2. User Obligations: By using Socialpound, you agree to provide accurate information and ensure the security of your account. You are responsible for maintaining the confidentiality of your login credentials.",
+  "3. Prohibited Activities: You may not use Socialpound for any unlawful purposes or in violation of any applicable laws. Engaging in unauthorized access, hacking, or any activity that may harm the platform or other users is strictly prohibited.",
+  "4. Data and Content: The data and content shared on Socialpound are strictly for educational purposes. You understand that any data you share on the platform may be used in this context and not for commercial purposes.",
+  "5. Termination of Access: We reserve the right to terminate or suspend your access to Socialpound at any time if you violate these terms of use.",
+  "6. Limitation of Liability: Socialpound is provided 'as is' for educational purposes, without warranties of any kind. We are not liable for any damages resulting from your use of the platform, including data loss or service disruptions.",
+  "7. Modifications to the Terms: We may update these terms at any time. Continued use of the platform after changes are made signifies your acceptance of the new terms.",
+  "8. Contact Information: If you have any questions or concerns about these terms, please contact us at souravscchoudhary@gmail.com",
+];
