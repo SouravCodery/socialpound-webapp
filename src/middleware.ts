84932e8e89354629d3ec1d272c27569f0e9fd0d1
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-03 02:23:57 +0530

    feat: Add isPublicRoute check in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { nextUrl } = request;
 
+  const isPublicRoute = [
+    "/about-us",
+    "/privacy-policy",
+    "/terms-of-service",
+  ].includes(nextUrl.pathname);
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
+
   const serverToken = request.cookies.get("server-token")?.value;
 
   const isLoggedIn = !!serverToken;
