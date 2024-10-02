c7256cd2b9925160ecc5f3ee47c9ba078b702868
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-03 02:32:11 +0530

    feat: Handle /public route in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,27 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { nextUrl } = request;
 
-  const isPublicRoute = [
-    "/about-us",
-    "/privacy-policy",
-    "/terms-of-service",
-  ].includes(nextUrl.pathname);
+  const isPublicRoute = nextUrl.pathname.startsWith("/public");
+  const isAuthRoute = nextUrl.pathname.startsWith("/public/account");
 
-  if (isPublicRoute) {
+  if (isPublicRoute && !isAuthRoute) {
     return NextResponse.next();
   }
 
   const serverToken = request.cookies.get("server-token")?.value;
-
   const isLoggedIn = !!serverToken;
-  const isAuthRoute = nextUrl.pathname.startsWith("/account");
 
   if (isLoggedIn && isAuthRoute) {
     return Response.redirect(new URL("/", request.url));
   }
 
   if (!isLoggedIn && !isAuthRoute) {
-    return Response.redirect(new URL("/account/sign-in", request.url));
+    return Response.redirect(new URL("/public/account/sign-in", request.url));
   }
 
   return NextResponse.next();
