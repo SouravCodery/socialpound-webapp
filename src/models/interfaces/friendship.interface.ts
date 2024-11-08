730a2927ecb367f2eafc8d927c0fcb1f5bba9d39
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-08 13:10:18 +0530

    feat: Update FriendshipInterface

--- src/models/interfaces/friendship.interface.ts
+++ src/models/interfaces/friendship.interface.ts
@@ -3,9 +3,9 @@ import { SubDocumentUserInterface } from "./user.interface";
 export type FriendshipStatus = "requested" | "accepted" | "rejected";
 
 export interface FriendshipInterface {
-  _id: string;
-  requester: SubDocumentUserInterface;
-  receiver: SubDocumentUserInterface;
+  _id?: string;
+  requester?: SubDocumentUserInterface;
+  receiver?: SubDocumentUserInterface;
   status: FriendshipStatus;
 }
 
