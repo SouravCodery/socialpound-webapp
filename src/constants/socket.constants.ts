7ef6218995f07ec918719e6f282420ab2188c0ed
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-15 19:16:25 +0530

    feat: Add EVENTS in SocketConstants
    Events: CALL_ANSWER, INCOMING_ANSWER, ANSWER_CALL

--- src/constants/socket.constants.ts
+++ src/constants/socket.constants.ts
@@ -2,7 +2,12 @@ export const SocketConstants = {
   EVENTS: {
     CALL_FRIEND: "call-friend",
     CALL_FAILED: "call-failed",
+    CALL_ANSWER: "call-answer",
+
     INCOMING_CALL: "incoming-call",
+    INCOMING_ANSWER: "incoming-answer",
+
+    ANSWER_CALL: "answer-call",
 
     CONNECTION_ERROR: "connect_error",
     ERROR: "error",
