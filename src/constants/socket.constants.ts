05996a14bd0f49fc82421e5e51b72a9b8682ac49
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-16 21:10:47 +0530

    feat: Add events in SocketConstants
    Events: CALL_REJECTED, NEW_ICE_CANDIDATE_SENT, NEW_ICE_CANDIDATE_RECEIVED

--- src/constants/socket.constants.ts
+++ src/constants/socket.constants.ts
@@ -3,11 +3,15 @@ export const SocketConstants = {
     CALL_FRIEND: "call-friend",
     CALL_FAILED: "call-failed",
     CALL_ANSWER: "call-answer",
+    CALL_REJECTED: "call-rejected",
 
     INCOMING_CALL: "incoming-call",
     INCOMING_ANSWER: "incoming-answer",
 
     ANSWER_CALL: "answer-call",
+
+    NEW_ICE_CANDIDATE_SENT: "new-ice-candidate-sent",
+    NEW_ICE_CANDIDATE_RECEIVED: "new-ice-candidate-received",
 
     CONNECTION_ERROR: "connect_error",
     ERROR: "error",
