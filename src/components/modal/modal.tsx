f922c58da5d3612658d326f63ef31f51d3a7691f
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-19 22:44:55 +0530

    feat: Add curtain and main ExtraClasses in Modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -7,18 +7,38 @@ export const Modal = ({
   closeModal,
 
   children,
+
+  curtainExtraClasses,
+  mainExtraClasses,
 }: {
   isModalOpen: boolean;
   closeModal: () => void;
 
   children: React.ReactNode;
+
+  curtainExtraClasses?: string;
+  mainExtraClasses?: string;
 }) => {
   if (!isModalOpen) return null;
 
   return (
     <>
-      <div className={classes.curtain} onClick={closeModal} />
-      <div className={clsx(classes.main, "shadow")}>{children}</div>
+      <div
+        className={clsx(
+          classes.curtain,
+          curtainExtraClasses && curtainExtraClasses
+        )}
+        onClick={closeModal}
+      />
+      <div
+        className={clsx(
+          classes.main,
+          "shadow",
+          mainExtraClasses && mainExtraClasses
+        )}
+      >
+        {children}
+      </div>
     </>
   );
 };
