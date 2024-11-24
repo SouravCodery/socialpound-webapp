b69fa723530758a75fb9cbde4185d1366b3f1b62
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-24 14:00:27 +0530

    feat: Add enabled key in Sentry.init

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,6 +5,8 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
+
   dsn: "https://9b04075c233c2e32aac258c809405883@o4508346974142464.ingest.us.sentry.io/4508346978336768",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
